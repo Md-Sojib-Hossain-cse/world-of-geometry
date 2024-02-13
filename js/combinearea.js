9ef22986f16ccc9979afc1b1967f2fedcba59abc
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2024-02-13 23:53:15 +0600

    reusable function added

--- js/combinearea.js
+++ js/combinearea.js
@@ -19,6 +19,28 @@ function calculateParallelogramFunction(){
     const area = base * height;
     resultUpdate('parallelogram-area' , area);
 }
+//Rhombus area
+function calculateRhombusArea(){
+    const d1 = inputValueConverter('rhombus-d1');
+    const d2 = inputValueConverter('rhombus-d2');
+    const area = 0.5 * d1 * d2;
+    resultUpdate('rhombus-area', area);
+}
+//Pentagon area
+function calculatePentagonArea(){
+    const perimeter = inputValueConverter('pentagon-perimeter');
+    const apothem = inputValueConverter('pentagon-apothem');
+    const area = 0.5 * perimeter * apothem;
+    resultUpdate('pentagon-area' , area);
+}
+//Ellipse area
+function calculateEllipseArea(){
+    const aAxis = inputValueConverter('a-axis');
+    const bAxis = inputValueConverter('b-axis');
+    const tempArea = Math.PI * aAxis * bAxis;
+    const area = parseFloat(tempArea.toFixed(6));
+    resultUpdate('ellipse-area' , area);
+}
 
 function inputValueConverter(converterId){
     const inputId = document.getElementById(converterId);
