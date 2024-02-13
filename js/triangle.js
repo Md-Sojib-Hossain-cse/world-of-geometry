1503c50e6f2e414dc5bdd1644282a97c2b4a8838
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2024-02-13 20:55:49 +0600

    parallelogram function added

--- js/triangle.js
+++ js/triangle.js
@@ -25,3 +25,16 @@ function calculateRectangleFunction(){
     const area = width * length;
     document.getElementById('rectangle-area').innerText = area;
 }
+
+function calculateParallelogramFunction(){
+    const inputBase = document.getElementById('parallelogram-base');
+    const baseValue = inputBase.value;
+    const base = parseFloat(baseValue);
+
+    const inputHeight = document.getElementById('parallelogram-heigth');
+    const heightValue = inputHeight.value;
+    const height = parseFloat(heightValue);
+
+    const area = base * height ;
+    document.getElementById('parallelogram-area').innerText = area;
+}
