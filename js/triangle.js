e7782deae47536c61581f5fc50a09c9243b8db0b
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2024-02-13 20:35:33 +0600

    rectangle card added

--- js/triangle.js
+++ js/triangle.js
@@ -13,4 +13,15 @@ function calculateTriangleArea(){
     areaSpan.innerText = area;
 }
 
-function
+function calculateRectangleFunction(){
+    const inputWidth = document.getElementById('rectangle-width');
+    const widthValue = inputWidth.value;
+    const width = parseFloat(widthValue);
+    
+    const inputLength = document.getElementById('rectangle-length');
+    const lengthValue = inputLength.value;
+    const length = parseFloat(lengthValue);
+
+    const area = width * length;
+    document.getElementById('rectangle-area').innerText = area;
+}
